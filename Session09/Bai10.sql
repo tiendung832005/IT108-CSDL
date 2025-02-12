b75088968536c4808b46ea0153c6a0df842873c3
tiendung832005 <tiendungx832005@gmail.com>
2025-02-12 15:45:44 +0700

    bt ss9

--- Session09/Bai10.sql
+++ Session09/Bai10.sql
@@ -5,21 +5,3 @@ select productLine, sum(quantityOrdered * priceEach) as total_sales, sum(quantit
 from orderdetails
 join products p on orderdetails.productCode = p.productCode
 group by productLine;
-
-select productLine, total_sales, total_quantity
-from view_total_sales;
-
-select v.productLine, pl.textDescription, v.total_sales, v.total_quantity,
-    case
-        when v.total_quantity > 1000 then concat(substr(pl.textDescription, 1, 30), '...') 
-        else substr(pl.textDescription, 1, 30)
-    end as description_snippet,
-    case
-        when v.total_quantity > 1000 then v.total_sales / v.total_quantity * 1.1
-        when v.total_quantity between 500 and 1000 then v.total_sales / v.total_quantity
-        when v.total_quantity < 500 then v.total_sales / v.total_quantity * 0.9
-    end as sales_per_product
-from view_total_sales v
-join productlines pl on v.productLine = pl.productLine
-where v.total_sales > 2000000
-order by v.total_sales desc;
